998e80f3e0f21e43d800413f68eedbbd51fa0c86
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-16 17:33:12 +0100

    Create _abs function

--- main.h
+++ main.h
@@ -1,4 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 void _putchar(char);
+int _strlen(char *);
+int _abs(int d);
+char *_itoa(int a);
+int number_of_digits(unsigned int d);
+void reverse_string(char *);
+char *strcat(char *, char *);
 #endif
